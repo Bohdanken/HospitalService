75746968fbd611dfa1835c52ff65cc787d33b1dc
Батька програміст <shevchenkobogdan2004@gmail.com>
2023-10-05 14:35:54 +0100

    Removed boilercode

--- src/main/java/ukma/edu/ua/HospitalApp/models/BaseEntity.java
+++ src/main/java/ukma/edu/ua/HospitalApp/models/BaseEntity.java
@@ -1,25 +1,21 @@
 package ukma.edu.ua.HospitalApp.models;
 
 import java.io.Serializable;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
 	public boolean isNew() {
 		return this.id == null;
