e2ec5b16c0ef65ca4a109e166dc45555320106c1
ilyadubov2004@gmail.com <ilyadubov2004@gmail.com>
2023-10-13 00:10:26 +0200

    Added markers, updated layout and appenders, fixed bugs

--- src/main/java/ukma/edu/ua/HospitalApp/Application.java
+++ src/main/java/ukma/edu/ua/HospitalApp/Application.java
@@ -1,6 +1,5 @@
 package ukma.edu.ua.HospitalApp;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/ukma/edu/ua/HospitalApp/api/auth/AuthController.java
+++ src/main/java/ukma/edu/ua/HospitalApp/api/auth/AuthController.java
@@ -1,5 +1,9 @@
 package ukma.edu.ua.HospitalApp.api.auth;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
   private final AuthService authService;
 
+  private final Logger logger = LogManager.getLogger(AuthController.class);
+
+  private final Marker loggerMarker = MarkerManager.getMarker("REQUEST_STATUS");
+
   @Autowired
   public AuthController(AuthService authService) {
     this.authService = authService;
@@ -17,16 +25,19 @@ public class AuthController {
 
   @PostMapping("/login/patient")
   public AuthService.TokenResponse loginPatient() {
+    logger.warn(loggerMarker, "Patient has logged in");
     return authService.login();
   }
 
   @PostMapping("/login/doctor")
   public AuthService.TokenResponse loginDoctor() {
+    logger.warn(loggerMarker, "Doctor has logged in");
     return authService.login();
   }
 
   @PostMapping("/login/admin")
   public AuthService.TokenResponse loginAdmin() {
+    logger.warn(loggerMarker, "Admin has logged in");
     return authService.login();
   }
 }
