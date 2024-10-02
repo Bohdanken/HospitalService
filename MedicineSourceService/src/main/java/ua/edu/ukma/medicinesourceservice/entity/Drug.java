4556ba66fc473a21025715cc6aec346dd4681548
Bohdan Shevchenko <104861557+Bohdanken@users.noreply.github.com>
2024-10-02 15:19:26 +0100

    Drug microservice

---
+++ MedicineSourceService/src/main/java/ua/edu/ukma/medicinesourceservice/entity/Drug.java
@@ -0,0 +1,28 @@
+package ua.edu.ukma.medicinesourceservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Drug {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String brandName;
+
+    @Column(nullable = false)
+    private String genericName;
+
+    @Column(nullable = false)
+    private String producer;
+
+    @Column(length = 5000)
+    private String description;
+}
