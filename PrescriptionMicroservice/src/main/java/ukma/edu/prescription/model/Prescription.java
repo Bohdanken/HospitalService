e2cb94e3ccf5606f10630f539604b37fd662447e
ilyadubov2004@gmail.com <ilyadubov2004@gmail.com>
2024-10-10 19:30:31 +0200

    Added integration between 3 services

--- PrescriptionMicroservice/src/main/java/ukma/edu/prescription/model/Prescription.java
+++ PrescriptionMicroservice/src/main/java/ukma/edu/prescription/model/Prescription.java
@@ -1,15 +1,17 @@
 package ukma.edu.prescription.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+
 import java.util.Date;
 
 @Data
+@Builder
 @NoArgsConstructor
-@SuperBuilder
+@AllArgsConstructor
 @Entity
 @Table(name = "prescription")
 public class Prescription {
@@ -22,9 +24,9 @@ public class Prescription {
   private Date dateOfIssue;
 
   @Column(name = "patient_id", nullable = false)
-  private Long patientId;  // Storing the patient ID
+  private Long patientId;
 
-  @Lob  // Large text field for storing the prescription details
-  @Column(name = "prescription_details", nullable = false)
-  private String prescriptionDetails;  // Storing the prescription content as a string
+  @Lob
+  @Column(name = "prescription_details", nullable = false, columnDefinition = "BLOB")
+  private byte[] prescriptionDetails;
 }
