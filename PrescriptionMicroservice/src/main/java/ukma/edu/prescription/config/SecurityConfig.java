186932f7af33886d1427e0eb7e7d49ef87994d0e
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-10-17 16:00:07 -0400

    Fix security config for Prescription Microservice to make it work

--- PrescriptionMicroservice/src/main/java/ukma/edu/prescription/config/SecurityConfig.java
+++ PrescriptionMicroservice/src/main/java/ukma/edu/prescription/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,14 +31,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(o -> o.disable())
-                .headers(o -> o.frameOptions(f -> f.disable())) // Allow H2 console to be displayed in a frame
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(o -> o.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // Allow H2 console to be displayed in a frame
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
                         // Allow access to H2 console
                         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+                        // Secure /api/admin/** with API key
+                        .requestMatchers(new AntPathRequestMatcher("/api/admin/**")).authenticated()
+                        .anyRequest().permitAll()) // Permit other requests
+//                        .anyRequest().authenticated())
                 .addFilterBefore(apiKeyAuthFilter(), UsernamePasswordAuthenticationFilter.class).build();
     }
 
@@ -56,25 +62,44 @@ public class SecurityConfig {
         @Override
         protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
                 throws IOException, ServletException {
-            if (new AntPathRequestMatcher("/swagger-ui/**").matches(request) ||
-                    new AntPathRequestMatcher("/v3/api-docs/**").matches(request) ||
-                    new AntPathRequestMatcher("/h2-console/**").matches(request) || // Allow H2 console
-                    new AntPathRequestMatcher("/api/admin/**").matches(request)) {
-                chain.doFilter(request, response);
-                return;
-            }
+            if (new AntPathRequestMatcher("/api/admin/**").matches(request)) {
+                String requestApiKey = request.getHeader(API_KEY_HEADER);
 
-            String requestApiKey = request.getHeader(API_KEY_HEADER);
-            if (validApiKey.equals(requestApiKey)) {
-                PreAuthenticatedAuthenticationToken token = new PreAuthenticatedAuthenticationToken(
-                        new User("apiUser", "", new ArrayList<>()), null, new ArrayList<>());
-                SecurityContextHolder.getContext().setAuthentication(token);
-            } else {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+                if (validApiKey.equals(requestApiKey)) {
+                    PreAuthenticatedAuthenticationToken token = new PreAuthenticatedAuthenticationToken(
+                            new User("apiUser", "", new ArrayList<>()), null, new ArrayList<>());
+                    SecurityContextHolder.getContext().setAuthentication(token);
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
             }
 
             chain.doFilter(request, response);
         }
+
+//        @Override
+//        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+//                throws IOException, ServletException {
+//            if (new AntPathRequestMatcher("/swagger-ui/**").matches(request) ||
+//                    new AntPathRequestMatcher("/v3/api-docs/**").matches(request) ||
+//                    new AntPathRequestMatcher("/h2-console/**").matches(request) || // Allow H2 console
+//                    new AntPathRequestMatcher("/api/admin/**").matches(request)) {
+//                chain.doFilter(request, response);
+//                return;
+//            }
+//
+//            String requestApiKey = request.getHeader(API_KEY_HEADER);
+//            if (validApiKey.equals(requestApiKey)) {
+//                PreAuthenticatedAuthenticationToken token = new PreAuthenticatedAuthenticationToken(
+//                        new User("apiUser", "", new ArrayList<>()), null, new ArrayList<>());
+//                SecurityContextHolder.getContext().setAuthentication(token);
+//            } else {
+//                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//                return;
+//            }
+//
+//            chain.doFilter(request, response);
+//        }
     }
 }
